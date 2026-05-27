448e6ec6b88208cbabfab2a45ec4cf2fe1026147
PromethusVision <dragut@outlook.com.tr>
2026-05-27 20:38:15 +0300

    feat: attach category ids to home category items

--- lib/features/shop/presentation/widgets/home_categories.dart
+++ lib/features/shop/presentation/widgets/home_categories.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:t_store/core/common/view_models/horizontal_small_list_view_item_view_model.dart';
 import 'package:t_store/core/common/widgets/horizontal_small_list_view.dart';
 import 'package:t_store/core/utils/constants/image_strings.dart';
 import 'package:t_store/core/utils/constants/text_strings.dart';
+import 'package:t_store/features/shop/presentation/cubit/categories_cubit.dart';
+import 'package:t_store/features/shop/presentation/cubit/categories_state.dart';
 
 class HomeCategories extends StatelessWidget {
   const HomeCategories({
@@ -14,12 +17,27 @@ class HomeCategories extends StatelessWidget {
     const List<String> categoriesTitles = TTexts.categories;
     const List<String> categoriesImages = TImages.categoryIcons;
 
-    final List<HorizontalSmallListViewItemModel> items = List.generate(
+    final List<HorizontalSmallListViewItemModel> fallbackItems = List.generate(
         categoriesImages.length,
         (index) => HorizontalSmallListViewItemModel(
               title: categoriesTitles[index],
               image: categoriesImages[index],
             ));
+    final categoriesState = context.watch<CategoriesCubit>().state;
+    final List<HorizontalSmallListViewItemModel> items =
+        categoriesState is CategoriesLoaded && categoriesState.categories.isNotEmpty
+            ? List.generate(
+                categoriesState.categories.length,
+                (index) {
+                  final category = categoriesState.categories[index];
+                  return HorizontalSmallListViewItemModel(
+                    categoryId: category.id,
+                    title: category.name,
+                    image: categoriesImages[index % categoriesImages.length],
+                  );
+                },
+              )
+            : fallbackItems;
 
     return SizedBox(
       height: 100,
