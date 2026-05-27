40aec9dde30c93aacd59dbf0b632c480a3d58d13
PromethusVision <dragut@outlook.com.tr>
2026-05-27 20:52:11 +0300

    fix: load real category ids in home categories

--- lib/features/shop/presentation/widgets/home_categories.dart
+++ lib/features/shop/presentation/widgets/home_categories.dart
@@ -7,41 +7,61 @@ import 'package:t_store/core/utils/constants/text_strings.dart';
 import 'package:t_store/features/shop/presentation/cubit/categories_cubit.dart';
 import 'package:t_store/features/shop/presentation/cubit/categories_state.dart';
 
-class HomeCategories extends StatelessWidget {
+class HomeCategories extends StatefulWidget {
   const HomeCategories({
     super.key,
   });
+
+  @override
+  State<HomeCategories> createState() => _HomeCategoriesState();
+}
+
+class _HomeCategoriesState extends State<HomeCategories> {
+  @override
+  void initState() {
+    super.initState();
+    final cubit = context.read<CategoriesCubit>();
+    if (cubit.state is! CategoriesLoaded && cubit.state is! CategoriesLoading) {
+      cubit.getCategories();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     const List<String> categoriesTitles = TTexts.categories;
     const List<String> categoriesImages = TImages.categoryIcons;
 
-    final List<HorizontalSmallListViewItemModel> fallbackItems = List.generate(
-        categoriesImages.length,
-        (index) => HorizontalSmallListViewItemModel(
-              title: categoriesTitles[index],
-              image: categoriesImages[index],
-            ));
-    final categoriesState = context.watch<CategoriesCubit>().state;
-    final List<HorizontalSmallListViewItemModel> items =
-        categoriesState is CategoriesLoaded && categoriesState.categories.isNotEmpty
-            ? List.generate(
-                categoriesState.categories.length,
-                (index) {
-                  final category = categoriesState.categories[index];
-                  return HorizontalSmallListViewItemModel(
-                    categoryId: category.id,
-                    title: category.name,
-                    image: categoriesImages[index % categoriesImages.length],
-                  );
-                },
-              )
-            : fallbackItems;
+    final fallbackItems = List.generate(
+      categoriesImages.length,
+      (index) => HorizontalSmallListViewItemModel(
+        title: categoriesTitles[index],
+        image: categoriesImages[index],
+      ),
+    );
 
     return SizedBox(
       height: 100,
-      child: HorizontalSmallListView(items: items),
+      child: BlocBuilder<CategoriesCubit, CategoriesState>(
+        builder: (context, state) {
+          if (state is CategoriesLoaded && state.categories.isNotEmpty) {
+            final items = List.generate(
+              state.categories.length,
+              (index) {
+                final category = state.categories[index];
+                return HorizontalSmallListViewItemModel(
+                  categoryId: category.id,
+                  title: category.name,
+                  image: categoriesImages[index % categoriesImages.length],
+                );
+              },
+            );
+
+            return HorizontalSmallListView(items: items);
+          }
+
+          return HorizontalSmallListView(items: fallbackItems);
+        },
+      ),
     );
   }
 }
