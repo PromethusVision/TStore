ff48f2fdc52f8da12ce8467f2e41cb1dc99df42d
PromethusVision <dragut@outlook.com.tr>
2026-05-30 23:01:03 +0300

    feat: show cart badge from cart state

--- lib/core/common/widgets/cart_counter_icon.dart
+++ lib/core/common/widgets/cart_counter_icon.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:t_store/core/common/view_models/cart_counter_icon_view_model.dart';
 import 'package:t_store/core/utils/constants/colors.dart';
+import 'package:t_store/features/cart/presentation/cubit/cart_cubit.dart';
+import 'package:t_store/features/cart/presentation/cubit/cart_state.dart';
 
 class CartCounterIcon extends StatelessWidget {
   const CartCounterIcon({
@@ -11,37 +14,45 @@ class CartCounterIcon extends StatelessWidget {
   final CartCounterIconModel cartCounterIconModel;
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        IconButton(
-          onPressed: cartCounterIconModel.onPressed,
-          icon: Icon(
-            Iconsax.shopping_bag,
-            color: cartCounterIconModel.color,
-          ),
-        ),
-        Positioned(
-          top: 0,
-          right: 0,
-          child: Container(
-            width: 18,
-            height: 18,
-            decoration: const BoxDecoration(
-              color: TColors.black,
-              shape: BoxShape.circle,
-            ),
-            child: Center(
-              child: Text(
-                cartCounterIconModel.count.toString(),
-                style: Theme.of(context)
-                    .textTheme
-                    .labelLarge!
-                    .apply(color: TColors.white, fontSizeFactor: .8),
+    return BlocBuilder<CartCubit, CartState>(
+      builder: (context, state) {
+        final count = state is CartLoaded
+            ? state.itemCount
+            : cartCounterIconModel.count ?? 0;
+
+        return Stack(
+          children: [
+            IconButton(
+              onPressed: cartCounterIconModel.onPressed,
+              icon: Icon(
+                Iconsax.shopping_bag,
+                color: cartCounterIconModel.color,
               ),
             ),
-          ),
-        ),
-      ],
+            Positioned(
+              top: 0,
+              right: 0,
+              child: Container(
+                width: 18,
+                height: 18,
+                decoration: const BoxDecoration(
+                  color: TColors.black,
+                  shape: BoxShape.circle,
+                ),
+                child: Center(
+                  child: Text(
+                    count.toString(),
+                    style: Theme.of(context)
+                        .textTheme
+                        .labelLarge!
+                        .apply(color: TColors.white, fontSizeFactor: .8),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
